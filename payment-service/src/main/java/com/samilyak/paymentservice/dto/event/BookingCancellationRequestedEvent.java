96cfa9ece771907eb566b6b5ddf8316bba5f2190
ilko-ilya <samilyakilya@gmail.com>
2026-01-31 13:01:27 +0200

    Fix: Add Jackson annotations to DTOs for correct Kafka serialization

--- payment-service/src/main/java/com/samilyak/paymentservice/dto/event/BookingCancellationRequestedEvent.java
+++ payment-service/src/main/java/com/samilyak/paymentservice/dto/event/BookingCancellationRequestedEvent.java
@@ -1,13 +1,25 @@
 package com.samilyak.paymentservice.dto.event;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+
 import java.time.LocalDate;
 
 public record BookingCancellationRequestedEvent(
 
         Long bookingId,
         Long accommodationId,
+
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        @JsonSerialize(using = LocalDateSerializer.class)
         LocalDate checkInDate,
+
+        @JsonDeserialize(using = LocalDateDeserializer.class)
+        @JsonSerialize(using = LocalDateSerializer.class)
         LocalDate checkOutDate,
+
         String paymentId,
         boolean refundNeeded
 
